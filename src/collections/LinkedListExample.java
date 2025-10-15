22cfe307b8389b45c0fa3dcf83e7154ca596898e
Vijay Sen Singh Yadav <91178268+vijaytech0102@users.noreply.github.com>
2025-10-15 11:38:23 +0530

    LinkedListExample

---
+++ src/collections/LinkedListExample.java
@@ -0,0 +1,20 @@
+package collections;
+
+import java.util.LinkedList;
+
+public class LinkedListExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//dynamic
+		
+		// doubly linked list implemented
+		LinkedList<String> list= new LinkedList<>();
+		
+		list.add("Satyam");
+		list.add("karan");
+		list.add("Rahul");
+	
+	}
+
+}
