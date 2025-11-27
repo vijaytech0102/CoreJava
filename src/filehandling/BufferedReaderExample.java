e38cf5731e34aa9b5ee9f4d5dd6cc509dd30db3f
Vijay Sen Singh Yadav <91178268+vijaytech0102@users.noreply.github.com>
2025-11-27 12:12:18 +0530

    file handling question

--- src/filehandling/BufferedReaderExample.java
+++ src/filehandling/BufferedReaderExample.java
@@ -9,18 +9,26 @@ public class BufferedReaderExample {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		try {
-			BufferedReader br = new BufferedReader(new FileReader("example.txt"));
+			BufferedReader br = new BufferedReader(new FileReader("file1.txt"));
 			String line;
-			int count=0;
+			int countLine=0;
+			int countword=0;
+			int charcount=0;
 			while ((line = br.readLine()) != null) {
 				System.out.println(line);
-				count++;
+				charcount+=line.length();
+				String arr[]= line.split(" ");
+				countword+=arr.length;
+				countLine++;
 			}
-			System.out.println("Number of lines in the file:"+count);
+			System.out.println("Number of lines in the file:"+countLine);
+			System.out.println("Number of word present in the File:"+countword);
+			System.out.println("Number of character present in the file:"+charcount);
 			br.close();
 		} catch (IOException e) {
 			System.out.println("Error reading file.");
 			e.printStackTrace();
+			e.getStackTrace();
 		}
 	}
 
